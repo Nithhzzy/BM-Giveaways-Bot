f63f5578f5b4673cde792ea5eeac467c610ab9f0
Nithh <114797570+Nithhzzy@users.noreply.github.com>
2024-05-28 04:03:25 +0700

    Discord.js v14

--- commands/start-giveaway.js
+++ commands/start-giveaway.js
@@ -1,11 +1,18 @@
 const Discord = require('discord.js');
+const ms = require('ms');
 const messages = require("../utils/messages");
 
 module.exports = {
 
-    description: 'Create a drop giveaway',
+    description: 'Start a giveaway',
 
     options: [
+        {
+            name: 'duration',
+            description: 'How long the giveaway should last for. Example values: 1m, 1h, 1d',
+            type: Discord.ApplicationCommandOptionType.String,
+            required: true
+        },
         {
             name: 'winners',
             description: 'How many winners the giveaway should have',
@@ -37,31 +44,33 @@ module.exports = {
         }
     
         const giveawayChannel = interaction.options.getChannel('channel');
+        const giveawayDuration = interaction.options.getString('duration');
         const giveawayWinnerCount = interaction.options.getInteger('winners');
         const giveawayPrize = interaction.options.getString('prize');
-    
+        
         if(!giveawayChannel.isTextBased()) {
             return interaction.reply({
                 content: ':x: Selected channel is not text-based.',
                 ephemeral: true
             });
         }
-
+    
         // Start the giveaway
         client.giveawaysManager.start(giveawayChannel, {
-            // The number of winners for this drop
-            winnerCount: giveawayWinnerCount,
-            // The prize of the giveaway
+            // The giveaway duration
+            duration: ms(giveawayDuration),
+            // The giveaway prize
             prize: giveawayPrize,
+            // The giveaway winner count
+            winnerCount: giveawayWinnerCount,
             // Who hosts this giveaway
             hostedBy: client.config.hostedBy ? interaction.user : null,
-            // specify drop
-            isDrop: true,
             // Messages
             messages
         });
     
         interaction.reply(`Giveaway started in ${giveawayChannel}!`);
+    
+    } 
 
-    }
 };
